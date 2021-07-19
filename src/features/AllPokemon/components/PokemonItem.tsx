6aabc310f3e75cd0b790c34bd8147527bef8aa94
Amit Nino <amitnino@moveo.co.il>
2021-07-19 16:21:10 +0300

    added-click-func-to-allpokemon

--- src/features/AllPokemon/components/PokemonItem.tsx
+++ src/features/AllPokemon/components/PokemonItem.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import { generatePokemonIdString } from '../../../utils/utils';
 import '../styles/PokemonItem.css';
 import { PokemonItemInterface } from '../utils/AllPokemonFeatureInterfaces';
@@ -11,8 +12,13 @@ const PokemonItemComponent: React.FC<Props> = ({pokemon}: Props) => {
 
     const { id, name, imageUrl } = pokemon;
 
+    const history = useHistory();
+    const onPokemonClick = () => {
+        history.push('/pokemon/'+pokemon.id);
+    };
+
     return (
-        <div className="container-pokemon-item" >
+        <div className="container-pokemon-item" id="hoverable-container" onClick={onPokemonClick }>
             <p id="id" >{generatePokemonIdString(id)}</p>
             <img id="pokemon" src={imageUrl} alt={`${name}`} />
             <p id="name" >{name}</p>
