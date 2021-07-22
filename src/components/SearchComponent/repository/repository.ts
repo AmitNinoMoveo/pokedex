41af2d5f9e51f5f9a46c3147b595052ff608e56f
Amit Nino <amitnino@moveo.co.il>
2021-07-22 10:08:02 +0300

    changed data recieved from api

--- src/components/SearchComponent/repository/repository.ts
+++ src/components/SearchComponent/repository/repository.ts
@@ -5,7 +5,7 @@ import { store } from "../../../state/store";
 interface rawResponse {
   data: {
     name: string,
-    order: number,
+    id: number,
   }
 }
 
@@ -16,7 +16,7 @@ export const fetchPokemonBySearch = async (): Promise<
     const query = store.getState().searchState;
     const { data } : rawResponse = await api.get("pokemon/" + query);
     const name = data.name.charAt(0).toUpperCase() + data.name.substring(1);
-    const id = data.order;
+    const id = data.id;
     const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
     const pokemonItem = [{ id, name, imageUrl }] as PokemonItemInterface[];
     return pokemonItem;
